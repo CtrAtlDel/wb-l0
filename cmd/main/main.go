c4a46fa1176d9273475de9c8da9cd0860c4b219e
CtrAtlDelete <kvasovski34@gmail.com>
2023-02-10 21:18:19 +0300

    test subscriber

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"github.com/nats-io/stan.go"
 	c "ivankvasov/project/internal/config"
 	"ivankvasov/project/internal/controller"
 	"ivankvasov/project/internal/model"
 	m "ivankvasov/project/internal/model"
+	"log"
 	"net/http"
 )
 
@@ -25,6 +27,16 @@ func main() {
 	router.HandleFunc("/models/{id}", controller.GetModelHandler).Methods("GET")
 	router.HandleFunc("/model", controller.PostModelHandler).Methods("POST")
 	http.Handle("/", router)
+	sc, err := stan.Connect("test-cluster", "13", stan.NatsURL("nats://localhost:4222"))
+	if err != nil {
+		log.Println(err)
+	}
+	defer sc.Close()
+	if _, err := sc.Subscribe("my-channel", func(m *stan.Msg) {
+		log.Printf("Received message: %s", string(m.Data))
+	}); err != nil {
+		log.Fatalf("Error subscribing to channel: %v", err)
+	}
 
 	http.ListenAndServe(":8181", nil)
 }
